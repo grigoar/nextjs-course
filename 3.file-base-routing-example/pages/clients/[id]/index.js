9d91959f9c4e6e8daee9bfd56a05f9ca903c6f0a
grigoar <grigore.nath.j@gmail.com>
2022-01-07 07:42:55 +0200

    navigating with link, 404 file

--- 3.file-base-routing-example/pages/clients/[id]/index.js
+++ 3.file-base-routing-example/pages/clients/[id]/index.js
@@ -4,10 +4,22 @@ function ClientProjectsPage() {
   const router = useRouter();
 
   console.log(router.query);
-  console.log(router.getInitialProps);
+
+  //buttons can be used after submitting a form
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/page");
+    // at replace the back will not function
+    // router.replace("/clients/max/page");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projectA" },
+    });
+  }
   return (
     <div>
       <h1>The Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
--- 3.file-base-routing-example/pages/index.js
+++ 3.file-base-routing-example/pages/index.js
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
       <ul>
         <li>
-          <a href='/portfolio'>Portfolio</a>
+          <Link href='/portfolio'>Portfolio</Link>
+        </li>
+        <li>
+          <Link href='/clients'>Clients</Link>
         </li>
       </ul>
     </div>
