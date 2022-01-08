94fd58415279a3b3f9346194826b97a6721c3ac5
grigoar <grigore.nath.j@gmail.com>
2022-01-08 11:56:49 +0200

    added Static generation, server side,  pre rendering

--- 5.pre-rendering&data-fetching-example/pages/index.js
+++ 5.pre-rendering&data-fetching-example/pages/index.js
@@ -1,11 +1,33 @@
+import fs from "fs/promises";
+import path from "path";
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+//this is executed first and then the component function
+export async function getStaticProps() {
+  console.log("Regenerating...");
+  //process current working directory
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  // console.log(data.products);
+  return {
+    props: {
+      // products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
