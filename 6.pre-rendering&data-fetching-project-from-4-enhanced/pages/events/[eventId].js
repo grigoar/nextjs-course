f31fc34266b050c466d618f4ff4a0b657c37b0d9
grigoar <grigore.nath.j@gmail.com>
2022-01-10 07:44:09 +0200

    added static  props, added get static paths, added server side props

--- 6.pre-rendering&data-fetching-project-from-4-enhanced/pages/events/[eventId].js
+++ 6.pre-rendering&data-fetching-project-from-4-enhanced/pages/events/[eventId].js
@@ -1,5 +1,9 @@
 import { useRouter } from "next/router";
-import { getEventById } from "../../dummy-data";
+import {
+  getEventById,
+  getAllEvents,
+  getFeaturedEvents,
+} from "../../helpers/api-util";
 import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
@@ -7,18 +11,19 @@ import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 import Button from "../../components/ui/button";
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
+  // const router = useRouter();
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+  // const eventId = router.query.eventId;
+  // const event = getEventById(eventId);
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
       <>
-        <ErrorAlert>
-          <p>No event found!</p>
-        </ErrorAlert>
+        <div className='center'>
+          <p>Loading...</p>
+        </div>
         <div className='center'>
           <Button link='/events'>Show All Events</Button>
         </div>
@@ -40,6 +45,31 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: { selectedEvent: event },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  // const events = await getAllEvents();
+  const events = await getFeaturedEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    //when fallback is true it will try to serve the page and then construct the path and the static page
+    // fallback: true,
+    //this is when we want to show  the page only the path is resolved and the static page is generated
+    fallback: "blocking",
+  };
 }
 
 export default EventDetailPage;
--- 6.pre-rendering&data-fetching-project-from-4-enhanced/pages/events/index.js
+++ 6.pre-rendering&data-fetching-project-from-4-enhanced/pages/events/index.js
@@ -1,13 +1,15 @@
 import { useRouter } from "next/router";
-import { getAllEvents } from "../../dummy-data";
+// import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { Fragment } from "react";
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  // const events = getAllEvents();
 
+  const { events } = props;
   function findEventsHandler(year, month) {
     console.log(year, month);
     const fullPath = `/events/${year}/${month}`;
@@ -21,6 +23,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
