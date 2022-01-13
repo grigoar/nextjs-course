3302d894e1def4f4f6e815df2deefb66e52c42e4
grigoar <grigore.nath.j@gmail.com>
2022-01-13 08:12:51 +0200

    started blog application- added layout, navigation, and hero section

---
+++ 10.blog-app-complete-project/pages/index.js
@@ -0,0 +1,16 @@
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
+}
+
+export default HomePage;
+
+// 1) Hero section > Present ourselves
+// 2) Featured Posts
