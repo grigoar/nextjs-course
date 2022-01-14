32d8b2034da1b6deb0113464c1f6ff39075aa004
grigoar <grigore.nath.j@gmail.com>
2022-01-14 06:58:52 +0200

    added featured posts, all posts, reading posts from filesystem

--- 10.blog-app-complete-project/pages/index.js
+++ 10.blog-app-complete-project/pages/index.js
@@ -1,48 +1,25 @@
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React application and sites a breeeze and ships with build-in SSR",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React application and sites a breeeze and ships with build-in SSR",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React application and sites a breeeze and ships with build-in SSR",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React application and sites a breeeze and ships with build-in SSR",
-    date: "2022-02-10",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
---
+++ 10.blog-app-complete-project/posts/getting-started-with-nextjs.md
@@ -0,0 +1,12 @@
+---
+title: Getting Started with NextJS
+date: "2022-10-15"
+author: grigoar
+image: getting-started-nextjs.png
+excerpt: "NextJS is the React framework for production - it makes building fullstack React application and sites a breeeze and ships with build-in SSR"
+isFeatured: true
+---
+
+# This a title
+
+This is a some regular text with a [this is a link](http://localhost:3000/posts/getting-started-with-nextjs)
