a5055bafcaa0de43d6b12d6b66b62696abff0d2f
grigoar <grigore.nath.j@gmail.com>
2022-01-14 20:05:59 +0200

    finished blog project, added notification on portal, header metadata, _document file

--- 10.blog-app-complete-project/pages/index.js
+++ 10.blog-app-complete-project/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +6,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Grigoar`s Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
